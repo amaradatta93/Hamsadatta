eb83e2cdb8962e4ea885c0aef06a5600079a3c73
Amara Datta <amaradatta@gmail.com>
2018-04-29 23:33:26 -0700

    restructure to and add source root

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, db_index=True)
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    content = models.TextField()
+    posted = models.DateField()
+    language = models.CharField(max_length=10)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 
+from blog.models import BlogPost
 
 def content(request):
+    blog = BlogPost()
     return HttpResponse('Hi my poems will come here')
