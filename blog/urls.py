1c1aab143bf69dbef9a1d5fe5b25baf8d41f151c
Amara Datta <amaradatta@gmail.com>
2018-05-09 13:46:25 -0700

    Fix calender and checkbox in add_post page

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('add_content', views.add_content, name='add_content'),
+    path('add_content', TemplateView.as_view(template_name='add_post.html'), name='add_post'),
     path('edit_content', views.edit_content, name='add_content'),
     path('delete_delete', views.delete_content, name='delete_content'),
     path('add_category', views.add_category, name='add_content'),
