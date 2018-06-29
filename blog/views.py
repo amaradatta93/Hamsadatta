9c6782243908edc25d5c1b9fa5fcaefb01299ba3
Amara Datta <amaradatta@gmail.com>
2018-06-28 23:41:46 -0700

    retrive title and slug only

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,22 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+import pprint
+
 
 from .models import Category, BlogPost
 
 
 def view_content(request):
-    posts = BlogPost.get_latest_by('title')
-    JsonResponse({'posts': posts})
+    posts = BlogPost.objects.all()
+    post_contents = list(posts.values('title', 'slug'))
+    # pprint.pprint(post_contents)
+    for values in post_contents:
+        print(values)
+    # return post_contents
 
 
 def view_category(request):
     # name_cat = list(Category.objects.filter(slug='asdfg'))
-    name_cat = get_object_or_404(Category)
-    print(name_cat)
-    return JsonResponse(name_cat)
+    # name_cat = get_object_or_404(Category)
+    # print(name_cat)
+    pass
