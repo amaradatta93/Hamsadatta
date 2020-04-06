e0de72523306bacf09289037aa576bf9421f5837
amaradatta93 <amaradatta@gmail.com>
2020-04-06 14:05:56 -0400

    Implement base class to handle the slug and dateformat for views

--- blog/views.py
+++ blog/views.py
@@ -6,25 +6,7 @@ from .forms import BlogPostForm
 from .models import BlogPost
 
 
-class BlogPostCreate(CreateView):
-    model = BlogPost
-    form_class = BlogPostForm
-
-    def get_form(self, form_class=None):
-        if form_class is None:
-            form_class = self.get_form_class()
-
-        form = super(BlogPostCreate, self).get_form(form_class)
-        form.fields['posted'].widget = forms.TextInput(attrs={'placeholder': 'mm/dd/yyyy'})
-        return form
-
-    def form_valid(self, form):
-        slug = str(form.instance.title).replace(" ", '-').lower()
-        form.instance.slug = slug
-        return super(BlogPostCreate, self).form_valid(form)
-
-
-class BlogPostUpdate(UpdateView):
+class BlogPostClean:
     model = BlogPost
     form_class = BlogPostForm
 
@@ -36,11 +18,20 @@ class BlogPostUpdate(UpdateView):
         if form_class is None:
             form_class = self.get_form_class()
 
-        form = super(BlogPostUpdate, self).get_form(form_class)
+        form = super().get_form(form_class)
         form.fields['posted'].widget = forms.TextInput(attrs={'placeholder': 'mm/dd/yyyy'})
         return form
 
     def form_valid(self, form):
         slug = str(form.instance.title).replace(" ", '-').lower()
         form.instance.slug = slug
-        return super(BlogPostUpdate, self).form_valid(form)
+        return super().form_valid(form)
+
+
+class BlogPostCreate(BlogPostClean, CreateView):
+    def get_object(self, *args, **kwargs):
+        pass
+
+
+class BlogPostUpdate(BlogPostClean, UpdateView):
+    pass
