24f27a1ef583a7fa0ea1a6c975f9e3df230770cc
amaradatta93 <amaradatta@gmail.com>
2020-05-12 19:39:31 -0400

    requirements.txt changed

--- darpana/settings.py
+++ darpana/settings.py
@@ -151,11 +151,11 @@ TEMPLATE_DIRS = (
     os.path.join(SETTINGS_PATH, 'templates'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "dist"),
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
